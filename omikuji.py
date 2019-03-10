2603fbb38a962bc4631f32c1e9442eb5316131ea
Kudo <kudo@example.com>
2019-03-10 09:36:30 +0900

    omikuji: random.randint

---
+++ omikuji.py
@@ -0,0 +1,15 @@
+omikuji = ["大吉", "吉", "凶"]
+
+# print(omikuji[0])
+
+import random
+
+idx = random.randint(0, 2)
+
+print(omikuji[idx])
+#
+# print(omikuji[random.randint(0, 2)])
+
+
+# ランダムにおみくじの結果を返すアプリを作る
+# ヒント: randomモジュールが使える
